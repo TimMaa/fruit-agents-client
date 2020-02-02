4bbcf52be811467437f0b060da733e4837b19e89
TimMaa <tim.maasen@ymail.com>
2020-02-02 10:30:08 +0100

    Layout agent overview with flex

---
+++ src/agents/agent-card.svelte
@@ -0,0 +1,56 @@
+<script>
+  import Icon from 'fa-svelte'
+  import { faAngleRight, faBaby } from '@fortawesome/free-solid-svg-icons';
+
+  export let agent;
+  let expanded = false;
+</script>
+
+<div class="agent-card">
+  <!-- {#each props as prop} -->
+    <div class="agent-card-image"><Icon icon={faBaby}></Icon></div>
+    <span class="agent-card-title">{agent.name}</span>
+    <button class="agent-card-expand"><Icon icon={faAngleRight}></Icon></button>
+  <!-- {/each} -->
+</div>
+
+<style>
+  .agent-card {
+    max-width: 100%;
+    min-width: 480px;
+    flex: 1;
+    height: 120px;
+    margin: 12px;
+    background: #0ff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .agent-card-title {
+    font-size: 2em;
+  }
+
+  .agent-card-image {
+    width: 90px;
+    align-self: stretch;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #999;
+    font-size: 5em;
+  }
+
+  .agent-card-image > * {
+    width: 100%;
+    height: 100%;
+  }
+
+  .agent-card-expand {
+    background: transparent;
+    border: none;
+    height: 100%;
+    margin: 0;
+    font-size: 2em;
+  }
+</style>
--- src/agents/agents-overview.svelte
+++ src/agents/agents-overview.svelte
@@ -1,7 +1,5 @@
 <script>
-  import Icon from 'fa-svelte'
-  import { faBaby } from '@fortawesome/free-solid-svg-icons';
-
+  import AgentCard from './agent-card.svelte';
   import { agents } from '../store.js';
 
   const props = [
@@ -15,43 +13,13 @@
 
 <div class="agent-container">
   {#each $agents as agent}
-    <div class="agent-card">
-      <!-- {#each props as prop} -->
-        <div class="agent-card-image"><Icon icon={faBaby}></Icon></div>
-        <div class="agent-card-title">{agent.name}</div>
-      <!-- {/each} -->
-    </div>
+    <AgentCard {agent}></AgentCard>
   {/each}
 </div>
-
-<!-- <div class="cell cell-foot header">Image</div> -->
 
 <style>
   .agent-container {
     display: flex;
     flex-flow: row wrap;
-  }
-
-  .agent-card {
-    max-width: 100%;
-    width: 360px;
-    height: 120px;
-    margin: 12px;
-    background: #0ff;
-    display: flex;
-  }
-
-  .agent-card-title {
-    font-size: 2em;
-  }
-
-  .agent-card-image {
-    width: 90px;
-    background: #999;
-  }
-
-  .agent-card-image > * {
-    width: 100%;
-    height: 100%;
   }
 </style>
